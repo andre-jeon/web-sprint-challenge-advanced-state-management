c15603e00d121c8c933ab8942cad7658a61cb050
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-14 13:43:57 -0400

    reducers added

--- smurfs/src/store/actions/smurfActions.js
+++ smurfs/src/store/actions/smurfActions.js
@@ -1,14 +1,35 @@
 import axios from 'axios'
 
+export const FETCH_DATA = "FETCH_DATA"
+export const FETCH_SUCCESS = "FETCH_SUCESS"
+export const FETCH_FAIL = "FETCH_FAIL"
+
 export const fetchSmurfs = () => (dispatch) => {
-    dispatch{{type:FETCH_DATA}}
+    dispatch({ type: FETCH_DATA })
     axios.get('http://localhost:3333/smurfs')
         .then(res => {
             console.log(res.data)
-            dispatch({type: FETCH_SUCESS, payload: res.data})
+            dispatch({ type: FETCH_SUCCESS, payload: res.data })
         })
         .catch(err => {
             console.log(err)
-            dispatch({type: FETCH_FAIL, payload: err})
+            dispatch({ type: FETCH_FAIL, payload: err })
+        })
+}
+
+export const POST_DATA = 'POST_DATA'
+export const POST_SUCCESS = 'POST_SUCESS'
+export const POST_FAIL = 'POST_FAIL'
+
+export const postSmurfs = (smurf) => (dispatch) => {
+    dispatch({ type: POST_DATA })
+    axios.get('http://localhost:3333/smurfs', smurf)
+        .then(res => {
+            console.log(res.data)
+            dispatch({ type: POST_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch({ type: POST_FAIL, payload: err })
         })
 }
